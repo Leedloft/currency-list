d9c2ea11e30f69453013148d9aa2d16c82c267d9
Leedloft <kenenbaevkema@gmail.com>
2023-01-10 12:31:15 +0600

    Begging

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  );
+}
+
+export default App;
+
+// 7sL7KRXD7I4BoNaWVPVdu5xaoLFQR6wz
